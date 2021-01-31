84cdd186d0b12adf2e8b1146be9b0a2f2e2a4eee
Cyclonus931 <78279071+Cyclonus931@users.noreply.github.com>
2021-01-31 23:23:18 +0200

    Update HW1.2.py

--- HW1.2.py
+++ HW1.2.py
@@ -2,11 +2,12 @@
 # Переведите время в часы, минуты и секунды и выведите в формате чч:мм:сс.
 # Используйте форматирование строк
 
-start_value = int(input('Hello! Please insert any time in seconds >>>  '))
+start_value = int(input('Hello! Please insert the any time in seconds >>>  '))
 hours = start_value // 3600
 minutes = start_value % 3600 // 60
 seconds = start_value % 3600 % 60
-print(f'The time is : {hours}h:{minutes}m:{seconds}s')
+
+print('The time is : {}h:{}m:{}s'.format('%02d' % hours, '%02d' % minutes, '%02d' % seconds))
 
 
 
