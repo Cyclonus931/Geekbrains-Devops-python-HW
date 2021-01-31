a1aac6ce40aee0424f1b523da044bc2b7d33612e
Cyclonus931 <78279071+Cyclonus931@users.noreply.github.com>
2021-01-31 21:05:35 +0200

    Add files via upload

---
+++ HW1.1.py
@@ -0,0 +1,14 @@
+#  Поработайте с переменными, создайте несколько, выведите на экран,
+#  запросите у пользователя несколько чисел и строк и сохраните в переменные, выведите на экран.
+
+a = 2
+b = 4
+c = b ** a
+d = "Hello yor value is = "
+print(d, c)
+
+a = input('Enter your name >>> ')
+b = int(input('Enter the year of your birth >>> '))
+c = 2020
+
+print(f'Hello {a} your age is {c - b} ')
