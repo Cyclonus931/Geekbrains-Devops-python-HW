0f01aa23108bf44dd002eab084a85fca05316d9e
Cyclonus931 <78279071+Cyclonus931@users.noreply.github.com>
2021-02-01 20:47:28 +0200

    Delete HW1.3.py

--- HW1.3.py
+++
@@ -1,6 +0,0 @@
-# Узнайте у пользователя число n. Найдите сумму чисел n + nn + nnn.
-# Например, пользователь ввёл число 3. Считаем 3 + 33 + 333 = 369.
-
-n = int(input('Insert the number >>> '))
-value = n + int(str(n) + str(n)) + int(str(n) + str(n) + str(n))
-print(f'{n} + {str(n) + str(n)} + {int(str(n) + str(n) + str(n))} = {value}')
